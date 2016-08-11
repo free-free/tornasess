5f4d2e9e42ef0b8abae33064fa79e460430ce4fb
John <19941222hb@gmail.com>
2016-08-11 12:02:04 +0800

    import all sub module

---
+++ tornasess/__init__.py
@@ -0,0 +1,12 @@
+#-*- coding=utf-8 -*-
+
+__author___ = "HUANGBIAO"
+__email__  = "19941222hb@gmail.com"
+__version__ = "0.1"
+
+
+
+from .tornado_session import *
+from .tornado_hbredis import *
+
+
