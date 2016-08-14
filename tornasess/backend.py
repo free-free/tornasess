112cc8f26ef3e11f3b618b96f01ff285cf81c108
John <19941222hb@gmail.com>
2016-08-14 17:38:18 +0800

    add session disk store backend

--- tornasess/backend.py
+++ tornasess/backend.py
@@ -1,6 +1,9 @@
 # coding:utf-8
 
 import functools
+import shelve
+import os
+import time
 try:
     import cPickle as pickle
 except Exception:
@@ -9,6 +12,7 @@ except Exception:
 from  tornado import gen
 import asyncmc
 
+
 from .tornado_hbredis import TornadoHBRedis
 
 
@@ -16,10 +20,13 @@ def _serialize(func):
     @gen.coroutine
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # args[2] = s_str
+        # args[2] = s_dict
         args_list = list(args)
-        args_list[2] = pickle.dumps(args_list[2])
-        args = tuple(args_list)
+        if not args_list[2]:
+            args_list[2] = None
+        else:
+            args_list[2] = pickle.dumps(args_list[2])
+            args = tuple(args_list)
         result = yield func(*args, **kwargs)
         return result
     return wrapper
@@ -69,13 +76,9 @@ class SessionStore(object, metaclass=SessionStoreMeta):
                  store class
         """
         store_class_name = backend_name.lower()+"sessionstore"
-        if not isinstance(config, dict):
-            config = {}
         store_instance = None
-        host = config.get("host")
-        port = config.get("port")
         try:
-            store_instance = cls._store_class.get(store_class_name)(host, port)
+            store_instance = cls._store_class.get(store_class_name)(config)
         except Exception:
             raise Exception("Can't resolve '%s' store instance " % backemd_name)
         return store_instance
@@ -83,10 +86,11 @@ class SessionStore(object, metaclass=SessionStoreMeta):
 
 class RedisSessionStore(SessionStore):
     
-    def __init__(self, host=None, port=None, autoconnect=True):
-        self._host = host or "localhost"
-        self._port = port or 6379
-        self._autoconnect = autoconnect
+    def __init__(self, config=None):
+        config = config or {}
+        self._host = config.get("host", "localhost")
+        self._port = config.get("port", 6379)
+        self._autoconnect = config.get("autoconnect", True)
         self._redis = TornadoHBRedis(self._host, self._port, self._autoconnect)
     
     @_serialize
@@ -113,9 +117,10 @@ class RedisSessionStore(SessionStore):
 
 class MemcacheSessionStore(SessionStore):
     
-    def __init__(self, hosts=None, port=None):
-        hosts = hosts or "localhost"
-        port = port or 11211
+    def __init__(self, config=None):
+        config = config or {}
+        hosts = config.get("host", "localhost")
+        port = config.get("port", 11211)
         if isinstance(hosts, str):
             servers = [hosts + ":" + str(port),]
         elif isinstance(hosts, (tuple, list)):
@@ -144,4 +149,54 @@ class MemcacheSessionStore(SessionStore):
       
      
 
-         
+class DiskSessionStore(SessionStore):
+    
+    def __init__(self, config=None):
+        config = config or {}
+        root = config.get("root", '/tmp')
+        if not os.path.exists(root):
+            os.makedirs(
+                os.path.abspath(root)
+            )
+        self.root = root
+
+    def _get_key_path(self, key):
+        if os.path.sep in key:
+            raise ValueError("Bad key:5s" % repr(key))
+        return os.path.join(self.root, key)
+
+    @_serialize
+    @gen.coroutine
+    def store(self, key, s_str, expires=0):
+        key_path = self._get_key_path(key)
+        if not s_str and os.path.exists(key_path):
+            os.remove(key_path)
+            return None
+        dump = {}
+        dump['data'] = s_str
+        if int(expires) == 0:
+            dump['expires'] = 0
+        else:
+            dump['expires'] = abs(expires) + time.time()
+        pickled = pickle.dumps(dump)
+        try:
+            with open(key_path, 'wb') as f:
+                f.write(pickled)
+        except IOError:
+             pass
+
+    @_unserialize
+    @gen.coroutine
+    def retrieve(self, key):
+        key_path = self._get_key_path(key)
+        if os.path.exists(key_path):
+            with open(key_path, 'rb') as f:
+                pickled = f.read() 
+            dump = pickle.loads(pickled)
+            if dump['expires'] > 0 and dump['expires'] < time.time():
+                os.remove(key_path)
+                return None
+            return dump.get('data')
+        else:
+            return None
+        
