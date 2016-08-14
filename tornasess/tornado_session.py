cb2ba1e0c9658e46ea157096b940f98eb844066d
John <19941222hb@gmail.com>
2016-08-14 15:56:17 +0800

    make big change to Session , SessionCacheFactory

--- tornasess/tornado_session.py
+++ tornasess/tornado_session.py
@@ -12,7 +12,7 @@ import tornadis
 from tornado import gen
 
 from .tornado_hbredis import TornadoHBRedis
-
+from .backend import SessionStore
 
 
 class AbstractSession(object):
@@ -126,48 +126,49 @@ class SessionNotStartError(Exception):
 
 class Session(AbstractSession):
 
-    def __init__(self, host, port, *, autoconnect=True, cache_factory=None):
-        super(RedisSession, self).__init__()
-        self._client = TornadoHBRedis(host, port, autoconnect)
+    def __init__(self, store_backend, cache_factory=None):
+        super(Session, self).__init__()
+        self._backend = store_backend
         self._session_data = dict()
         self._session_start_flag = False
         self._used_flag = False
         self._cache_factory = cache_factory
+
+    def __getitem__(self, key):
+        return self._session_data.get(key)
+
+    def __setitem__(self, key, value):
+        self._session_data[key] = value
+    
+    def __delitem__(self, key):
+        if key in self._session_data:
+            del self._session_data[key]
+
+    def __contains__(self, key):
+        return (key in self._session_data)
 
     def _check_session_start(self):
         if not self._session_start_flag:
             raise SessionNotStartError("session not start")
 
     def set(self, key, value):
-        self._check_session_start()
         self._session_data[key] = value
 
     def multi_set(self, pairs):
-        self._check_session_start()
         for k, v in pairs.items():
             self._session_data[k] = v
 
     def multi_get(self, key_l):
-        self._check_session_start()
         pairs = {}
         for k in key_l:
             pairs[k] = self._session_data.get(k)
         return pairs
 
     def get(self, key):
-        self._check_session_start()
         return self._session_data.get(key)
 
     def all(self):
         return self._session_data
-
-    def _list_to_dict(self, src_list):
-        dest_dict = {}
-        list_l = len(src_list)
-        for i in range(0, list_l, 2):
-            dest_dict[src_list[i].decode(
-                "utf-8")] = src_list[i + 1].decode("utf-8")
-        return dest_dict
 
     def refresh_sid(self, session_id=None):
         if session_id:
@@ -180,56 +181,23 @@ class Session(AbstractSession):
     def start(self, session_id=None):
         if session_id:
             self._session_id = session_id
-        session_data_byte = yield self._client.sget(self._session_id)
-        if not session_data_byte:
-            self._session_data = {}
-        else:
-            self._session_data = pickle.loads(session_data_byte) or {}
+        self._session_data = yield self._backend.retrieve(self._session_id)
         self._session_start_flag = True
 
     @gen.coroutine
-    def save(self, expire=0):
+    def end(self, expires=0):
         self._check_session_start()
-        if int(expire) > 0:
-            pipeline = self._client.pipeline()
-            pipeline.set(self._session_id, pickle.dumps(self._session_data))
-            pipeline.expire(self._session_id, int(expire))
-            result = yield pipeline.execute()
-            return result
-        else:
-            result = yield self._client.set(self._session_id, pickle.dumps(self._session_data))
-        return result
+        resp = yield self._backend.store(self._session_id, self._session_data, expires)
+        return resp
 
-    @gen.coroutine
-    def destroy(self, session_id=None):
-        if not session_id:
-            session_id = self._session_id
-            self._session_data = {}
-        result = yield self._client.delete(session_id)
-        return result
+    def destroy(self):
+        self._session_data = {}
 
-    @gen.coroutine
-    def delete(self, key, session_id=None):
-        result = None
-        if  session_id :
-            sess_data_byte = yield self._client.sget(session_id) 
-            if not sess_data_byte:
-                return result
-            sess_data = pickle.loads(sess_data_byte) 
-            if key in sess_data:
-                del sess_data[key]
-                result = yield self._client.set(sesion_id, pickle.dumps(sess_data))
-        else:
-            if key in self._session_data:
-                del self._session_data[key]
-                result = yield self._client.set(self._session_id, pickle.dumps(self._session_data))
-        return result
-
-    def __getitem__(self, key):
-        return self.get(key)
-
-    def __setitem__(self, key, value):
-        self.set(key, value)
+    def delete(self, key):
+        if key in self._session_data:
+            del self._session_data[key]
+            return True
+        return False
 
     @property
     def used_flag(self):
@@ -248,26 +216,23 @@ class Session(AbstractSession):
 
 class SessionCacheFactory(object):
 
-    def __init__(self, driver_name, host, port, min_cache=2, max_cache=3):
-        assert isinstance(driver_name, str)  # driver_name
-        assert isinstance(host, str)  # host
-        assert isinstance(port, int)  # port
+    def __init__(self, backend_name, config=None, min_cache=2, max_cache=3):
+        assert isinstance(backend_name, str)  # backend_name
         assert isinstance(min_cache, int)  # min_cache
         assert isinstance(max_cache, int)  # max_cache
         self.__session_cache = deque()
-        self.__driver_name = driver_name.lower()
-        self.__host = host
-        self.__port = port
+        self.__backend_name = backend_name
         self.__min_cache_size = min_cache
         self.__max_cache_size = max_cache
+        self.__config = config
         self.__cache_size = 0
         for i in range(0, self.__min_cache_size):
             self.__cache_size += 1
-            self.__session_cache.append(
-                getattr(self, 'get_%s_session' % self.__driver_name)(self.__host, self.__port))
+            self.__session_cache.append(self.resolve_session_instance(self.__backend_name, self.__config))
 
-    def get_redis_session(self, host, port):
-        return RedisSession(host, port, cache_factory=self)
+    def resolve_session_instance(self, backend_name, config):
+        store = SessionStore.resolve_store_instance(backend_name, config)
+        return Session(store, cache_factory=self)
 
     def get_session(self):
         self._check_cache()
@@ -288,8 +253,7 @@ class SessionCacheFactory(object):
         if self.__cache_size < self.__min_cache_size:
             cache_size = self.__cache_size
             for i in range(0, self.__max_cache_size - cache_size):
-                self._add_to_cache(getattr(self, 'get_%s_session' %
-                                           self.__driver_name)(self.__host, self.__port))
+                self._add_to_cache(self.resolve_session_instance(self.__backend_name,self.__config))
                 self.__cache_size += 1
 
     def _add_to_cache(self, session_instance):
