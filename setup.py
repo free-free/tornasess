05cbea845645f2467ead862b4e8425aaaf339c34
John <19941222hb@gmail.com>
2016-08-16 23:43:59 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # coding:utf-8
 from setuptools import setup,find_packages
+import re
 
-
-
+from tornasess import __version__
 
 setup(
     name="tornado_session",
-    version="0.1",
+    version=__version__,
     author="HuangBiao",
     author_email="19941222hb@gmail.com",
     description="A tornado session implementation",
     license="MIT",
-    package=find_packages(),
-    install_requires=['tornado', 'tornadis', 'asyncmc',],
+    packages=find_packages(),
+    install_requires=re.split(r"\r*\n+",open("requirements.txt","r").read())[0:-1],
     include_package_data=True
 )
