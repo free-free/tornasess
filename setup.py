08fa3c8d491af59fca71bfc85e81f1b9946c0eff
John <19941222hb@gmail.com>
2016-08-18 19:48:33 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,18 @@ setup(
     author_email="19941222hb@gmail.com",
     description="A tornado session implementation",
     license="MIT",
+    url="https://github.com/free-free/tornasess",
     packages=find_packages(),
     install_requires=re.split(r"\r*\n+",open("requirements.txt","r").read())[0:-1],
     include_package_data=True
+    keywords=['submail','sdk'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
