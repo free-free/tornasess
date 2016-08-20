26696d68e6d2cf0ac65556bd35729c1c7dbe3fe1
John <19941222hb@gmail.com>
2016-08-21 02:03:10 +0800

    change version number

--- tornasess/__init__.py
+++ tornasess/__init__.py
@@ -2,11 +2,10 @@
 
 __author___ = "HUANGBIAO"
 __email__  = "19941222hb@gmail.com"
-__version__ = "0.3"
+__version__ = "0.4"
 
 
 
 from .tornado_session import *
-from .tornado_hbredis import *
 
 
